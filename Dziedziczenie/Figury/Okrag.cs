394fb472e37310559c1810332eb20877f787c552
Twigen <twigen.zlotooki@gmail.com>
2020-04-25 14:26:59 +0200

    Update Okrag.cs

--- Dziedziczenie/Figury/Okrag.cs
+++ Dziedziczenie/Figury/Okrag.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//zmiana z poziomu githuba
 namespace Dziedziczenie.Figury
 {
     class Okrag
     {
+        
         private double srednica;
+        public double Oblicz()
+        {
+        }
     }
 }
