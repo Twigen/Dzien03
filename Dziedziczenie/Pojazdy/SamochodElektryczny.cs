06ad78003e8634e2f963d8a241fd111cd8f3016c
Norbert Wieczorek <twigen.zlotooki@gmail.com>
2020-04-25 14:18:19 +0200

    minor changes

--- Dziedziczenie/Pojazdy/SamochodElektryczny.cs
+++ Dziedziczenie/Pojazdy/SamochodElektryczny.cs
@@ -9,9 +9,10 @@ namespace Dziedziczenie.Pojazdy
     class SamochodElektryczny:Samochod
     {
         private double pojemnoscBaterii;
+        private double zywotnoscBaterii;
         void Zatankuj()
         {
-            Console.WriteLine("podlacz do pradu");
+            Console.WriteLine();
         }
 
     }
